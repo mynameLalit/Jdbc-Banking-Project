acfd0f47f1dfd86d410d17eb08ecad4fecbe2867
Shital <Shital@192.168.2.6>
2017-05-03 12:47:36 +0530

    Registration Controller test line commited

--- JDBC Banking Project/src/RegisterServlet.java
+++ JDBC Banking Project/src/RegisterServlet.java
@@ -27,6 +27,7 @@ public class RegisterServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Inide doGet method");
 	}
 
 	/**
@@ -34,7 +35,7 @@ public class RegisterServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println("Inside Post method()");
+		System.out.println("Inside doPost method");
 		PrintWriter out = response.getWriter();
 		out.print("<html><h3>It is coming from RegisterServlet.java</h3></html>");
 		String name = request.getParameter("name");
