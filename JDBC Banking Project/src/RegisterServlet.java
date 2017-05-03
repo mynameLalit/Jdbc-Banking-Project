bd5e8d74c6dc11aa9ffe9a31aca47b739dc1cc4e
Shital <Shital@192.168.2.6>
2017-05-03 12:19:55 +0530

    Initial checking of Jdbc banking project

---
+++ JDBC Banking Project/src/RegisterServlet.java
@@ -0,0 +1,123 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+//@WebServlet("/RegisterServletPath")
+@WebServlet(name="RegisterServlet", urlPatterns={"/RegisterServletPath"})
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Inside Post method()");
+		PrintWriter out = response.getWriter();
+		out.print("<html><h3>It is coming from RegisterServlet.java</h3></html>");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String mobile = request.getParameter("mobile");
+		String city = request.getParameter("city");
+		String dob = request.getParameter("dob");
+		
+		/*write the logic for input validation*/
+		String errorMsg = null;
+		if(name == null || name.equals("")) {
+			errorMsg = "Name can't be empty";
+		}
+		if(email == null || email.equals("")) {
+			errorMsg = "Email can't be empty";
+		}
+		if(mobile == null || mobile.equals("")) {
+			errorMsg = "Mobile number can't be empty";
+		}
+		if(city == null || city.equals("")) {
+			errorMsg = "City name can't be empty";
+		}
+		if(dob == null || dob.equals("")) {
+			errorMsg = "Date of birth can't be empty";
+		}
+		
+		System.out.println("Line 64");
+		
+		//when the text field is not null, this code statement won't run
+		if(errorMsg != null) {
+			System.out.println("Line 67");
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.html");
+			PrintWriter out1 = response.getWriter();
+			out1.println("<font color=red>" +errorMsg+"</font>");
+			rd.include(request, response);
+		}
+		
+	//	System.out.println("Line 73");
+		else {
+			System.out.println("Line 73");
+			Connection con = (Connection)getServletContext().getAttribute("DBConnection");
+			PreparedStatement ps = null;
+			System.out.println("Line 79");
+			
+			
+			try{
+				System.out.println("Line 81");
+				ps = con.prepareStatement("insert into customers(name,email,mobile,city,dob) values (?,?,?,?,?)");
+				System.out.println("Line 82");
+				ps.setString(1, name);
+				ps.setString(2, email);
+				ps.setString(3, mobile);
+				ps.setString(4, city);
+				ps.setString(5, dob);
+				System.out.println("Line 87");	
+				ps.execute();
+				
+				//forward to login page to login
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+				PrintWriter out1 = response.getWriter();
+				out1.println("<font color=green>Registration Successful, please login.</font>");
+				rd.include(request, response);
+				
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+				//logger.error("Database connection problem");
+				throw new ServletException("DB Connection Problem");
+			}
+			finally {
+				try {
+					ps.close();
+				}
+				catch(SQLException e) {
+					System.out.println("Inside second catch block");
+					//logger.error("SQLException is closing PreparedStatement");
+				}
+			}
+		}
+		
+		
+		//doGet(request, response);
+	}
+	
+
+}
---
+++ JDBC Banking Project/src/com/banking/util/DBConnectionManager.java
@@ -0,0 +1,25 @@
+package com.banking.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * DBConnectionManager.java is a utility class for Oracle11g database connection and it has a method that
+ * returns connection object. We will use this class for database connection and then set the connection
+ * to servlet context attribute(web.xml) that other servlets can use.
+ * */
+
+public class DBConnectionManager {
+	private Connection connection;
+	
+		public DBConnectionManager(String dbURL, String user, String password) throws 
+		ClassNotFoundException, SQLException {
+			
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				this.connection = DriverManager.getConnection(dbURL,user,password);
+		}
+		public Connection getConnection() {
+			return this.connection;
+		}
+}
