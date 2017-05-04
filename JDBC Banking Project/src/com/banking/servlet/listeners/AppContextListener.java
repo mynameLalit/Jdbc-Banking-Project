d9bcae8388fcdfb8a65ddb61305ddec82cb79f20
Shital <Shital@192.168.2.6>
2017-05-04 11:39:18 +0530

    servlet context listener is been created.

--- JDBC Banking Project/src/com/banking/servlet/listeners/AppContextListener.java
+++ JDBC Banking Project/src/com/banking/servlet/listeners/AppContextListener.java
@@ -1,5 +1,45 @@
 package com.banking.servlet.listeners;
 
-public class AppContextListener {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.banking.util.DBConnectionManager;
+
+public class AppContextListener implements ServletContextListener {
 	
+		public void contextInitialized(ServletContextEvent servletContextEvent) {
+			ServletContext ctx = servletContextEvent.getServletContext();
+			
+			//Initialize DB connection
+			String URL = ctx.getInitParameter("dbURL");
+			String USER = ctx.getInitParameter("username");
+			String PASSWORD = ctx.getInitParameter("password");
+			
+			try {
+				DBConnectionManager connectionManager = new DBConnectionManager(URL, USER, PASSWORD);
+				ctx.setAttribute("DBConnection", connectionManager.getConnection());
+				System.out.println("DB Connection Intialized Successfully");
+			}
+			catch(ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		public void contextDestroyed(ServletContextEvent servletContextEvent) {
+			Connection con = (Connection)servletContextEvent.getServletContext().getAttribute("DBConnection");
+			try {
+				con.close();
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 }
